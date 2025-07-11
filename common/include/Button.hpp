88495d78809cc2029b3da3567341bb9538095251
MrKeviscool <danielandkev@outlook.com>
2025-07-11 14:30:04 +0930

    button optimisation

--- common/include/Button.hpp
+++ common/include/Button.hpp
@@ -2,24 +2,24 @@
 
 #include "InputState.hpp"
 
-#include <SFML/Graphics.hpp>
-
 #include <functional>
 
-class Button{
+#include <SFML/Graphics.hpp>
+
+class Button {
 public:
     Button() = default;
     Button(const sf::Vector2f size);
     Button(const sf::Vector2f size, const sf::Vector2f position);
-    Button(const sf::Vector2f size, const sf::Vector2f position, std::function<void()> func);
+    Button(const sf::Vector2f size, const sf::Vector2f position, const std::function<void()>& func);
     Button(const sf::Vector2f size, const sf::Vector2f position, const sf::Texture* texture);
-    Button(const sf::Vector2f size, const sf::Vector2f position, const sf::Texture* texture, std::function<void()> func);
+    Button(const sf::Vector2f size, const sf::Vector2f position, const sf::Texture* texture, const std::function<void()>& func);
 
     void setPosition(const sf::Vector2f);
     void setSize(const sf::Vector2f);
     void setText(const std::string& text, const sf::Font* font, const unsigned int size = 12, const float offsetAmount = 2);
     void setTexture(const sf::Texture* texture);
-    void setFunction(std::function<void()> func);
+    void setFunction(const std::function<void()>& func);
 
     bool poll();
     
@@ -28,15 +28,13 @@ public:
     bool isHovering() const;
 
     const sf::RectangleShape& getShape() const;
-    const sf::RectangleShape& getTintShape() const;
 
 private:
     sf::RectangleShape m_shape;
-    sf::RectangleShape m_tintShape;
     bool m_hovering = false;
     InputState* m_input = nullptr;
     std::function<void()> m_clickFunc;
     sf::Text m_text;
 
-    const sf::Color tintColor = {127, 127, 127, 127};
+    const sf::Color m_tintColor = { 127, 127, 127 };
 };
--- common/src/Button.cpp
+++ common/src/Button.cpp
@@ -2,7 +2,7 @@
 
 #include "ResourceManager.hpp"
 
-Button::Button(const sf::Vector2f size) : m_shape(size), m_tintShape(size)
+Button::Button(const sf::Vector2f size) : m_shape(size)
 {
 
 }
@@ -11,41 +11,36 @@ Button::Button(const sf::Vector2f size, const sf::Vector2f position)
     : Button(size)
 {
     m_shape.setPosition(position);
-    m_tintShape.setPosition(position);
 }
 
-Button::Button(const sf::Vector2f size, const sf::Vector2f position, std::function<void()> func)
-    : m_shape(size), m_tintShape(size), m_clickFunc(func)
+Button::Button(const sf::Vector2f size, const sf::Vector2f position, const std::function<void()>& func)
+    : m_shape(size), m_clickFunc(func)
 {
     m_shape.setPosition(position);
-    m_tintShape.setPosition(position);
 }
 
 Button::Button(const sf::Vector2f size, const sf::Vector2f position, const sf::Texture* texture) 
     : Button(size, position) 
 {
     m_shape.setPosition(position);
-    m_tintShape.setPosition(position);
     m_shape.setTexture(texture);
 }
 
-Button::Button(const sf::Vector2f size, const sf::Vector2f position, const sf::Texture* texture, std::function<void()> func) 
-    : m_shape(size), m_tintShape(size), m_clickFunc(func)
+Button::Button(const sf::Vector2f size, const sf::Vector2f position, const sf::Texture* texture, const std::function<void()>& func) 
+    : m_shape(size), m_clickFunc(func)
 {
     m_shape.setPosition(position);
-    m_tintShape.setPosition(position);
     m_shape.setTexture(texture);
 }
 
 void Button::setPosition(const sf::Vector2f pos){
     m_shape.setPosition(pos);
-    m_tintShape.setPosition(pos);
 }
 
 void Button::setSize(const sf::Vector2f size){
     m_shape.setSize(size);
-    m_tintShape.setSize(size);
 }
+
 
 void Button::setText(const std::string& text, const sf::Font* font, const unsigned int size, const float offsetAmount){
     m_text = sf::Text(text, *font, size);
@@ -62,19 +57,17 @@ bool Button::poll(){
         && m_input->mousePos().y < m_shape.getPosition().y  + m_shape.getSize().y);
     
     if(!isHovering()){
-        m_tintShape.setFillColor({0,0,0,0});
+        m_shape.setFillColor({ 255, 255, 255 });
         return false;
     }
-
-    m_tintShape.setFillColor(tintColor);
+    
+    m_shape.setFillColor(m_tintColor);
 
     if(m_input->mouseEvents().find({sf::Mouse::Button::Left, InputState::ButtonState::pressed}) != m_input->mouseEvents().end()){
         if(m_clickFunc) m_clickFunc();
         return true;
     }
-
     return false;
-
 }
 
 bool Button::isHovering() const {
@@ -85,13 +78,8 @@ const sf::RectangleShape& Button::getShape() const {
     return m_shape;
 }
 
-const sf::RectangleShape& Button::getTintShape() const {
-    return m_tintShape;
-}
-
 void Button::draw(sf::RenderWindow& target) const {
     target.draw(m_shape);
-    target.draw(m_tintShape);
     target.draw(m_text);
 }
 
@@ -99,6 +87,6 @@ void Button::setTexture(const sf::Texture* texture) {
     m_shape.setTexture(texture);
 }
 
-void Button::setFunction(std::function<void()> func) {
+void Button::setFunction(const std::function<void()>& func) {
     m_clickFunc = func;
 }
